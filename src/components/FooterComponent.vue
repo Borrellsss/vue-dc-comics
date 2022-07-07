2f98c8749edd1de1b7b244d914cdd063ce0ce0ab
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-07-07 18:38:47 +0200

    footer bottom done

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -7,105 +7,105 @@
                 <div class="footer-top-left flex">
                     <div class="footer-top-left-merch">
                         <!-- dc comics -->
-                        <ul>
-                            <li>
-                                <h4>DC COMICS</h4>
-                            </li>
-                            <li>
-                                <a href="#">Charcters</a>
-                            </li>
-                            <li>
-                                <a href="#">Comics</a>
-                            </li>
-                            <li>
-                                <a href="#">Movies</a>
-                            </li>
-                            <li>
-                                <a href="#">TV</a>
-                            </li>
-                            <li>
-                                <a href="#">Games</a>
-                            </li>
-                            <li>
-                                <a href="#">Videos</a>
-                            </li>
-                            <li>
-                                <a href="#">News</a>
-                            </li>
-                        </ul>
+                        <div class="dc-comics">
+                            <h4>DC COMICS</h4>
+                            <ul>
+                                <li>
+                                    <a href="#">Charcters</a>
+                                </li>
+                                <li>
+                                    <a href="#">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="#">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="#">TV</a>
+                                </li>
+                                <li>
+                                    <a href="#">Games</a>
+                                </li>
+                                <li>
+                                    <a href="#">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                            </ul>
+                        </div>
                         <!-- shop -->
+                        <div class="shop">
+                            <h4>SHOP</h4>
+                            <ul>
+                                <li>
+                                    <a href="#">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- dc -->
+                    <div class="dc">
+                        <h4>DC</h4>
                         <ul>
                             <li>
-                                <h4>SHOP</h4>
+                                <a href="#">Terms Of Use</a>
                             </li>
                             <li>
-                                <a href="#">Shop DC</a>
+                                <a href="#">Privacy Policy (New)</a>
                             </li>
                             <li>
-                                <a href="#">Shop DC Collectibles</a>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
                             </li>
                         </ul>
                     </div>
-                    <!-- dc -->
-                    <ul>
-                        <li>
-                            <h4>DC</h4>
-                        </li>
-                        <li>
-                            <a href="#">Terms Of Use</a>
-                        </li>
-                        <li>
-                            <a href="#">Privacy Policy (New)</a>
-                        </li>
-                        <li>
-                            <a href="#">Ad Choices</a>
-                        </li>
-                        <li>
-                            <a href="#">Advertising</a>
-                        </li>
-                        <li>
-                            <a href="#">Jobs</a>
-                        </li>
-                        <li>
-                            <a href="#">Subscriptions</a>
-                        </li>
-                        <li>
-                            <a href="#">Talent Workshops</a>
-                        </li>
-                        <li>
-                            <a href="#">CPSC Certificates</a>
-                        </li>
-                        <li>
-                            <a href="#">Ratings</a>
-                        </li>
-                        <li>
-                            <a href="#">Shop Help</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact Us</a>
-                        </li>
-                    </ul>
                     <!-- sites -->
-                    <ul>
-                        <li>
-                            <h4>SITES</h4>
-                        </li>
-                        <li>
-                            <a href="#">DC</a>
-                        </li>
-                        <li>
-                            <a href="#">MAD Magazine</a>
-                        </li>
-                        <li>
-                            <a href="#">DC Kids</a>
-                        </li>
-                        <li>
-                            <a href="#">DC Universe</a>
-                        </li>
-                        <li>
-                            <a href="#">DC Power Visa</a>
-                        </li>
-                    </ul>
+                    <div class="sites">
+                        <h4>SITES</h4>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="footer-top-right">
                     
@@ -116,7 +116,45 @@
     <!-- footer bottom section -->
     <div class="footer-bottom">
         <div class="container">
-            
+            <div class="flex">
+                <div class="footer-bottom-left">
+                    <a href="#" class="btn blue">SIGN-UP NOW!</a>
+                </div>
+                <div class="footer-bottom-right">
+                    <div class="sites flex">
+                        <span>FOLLOW US</span>
+                        <div class="sites-links">
+                            <ul class="flex">
+                                <li>
+                                    <a href="">
+                                        <img src="../assets/img/footer-facebook.png" alt="facebook logo">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <img src="../assets/img/footer-twitter.png" alt="twitter logo">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <img src="../assets/img/footer-youtube.png" alt="youtube logo">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <img src="../assets/img/footer-pinterest.png" alt="pinterest logo">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <img src="../assets/img/footer-periscope.png" alt="periscope logo">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
@@ -143,14 +181,16 @@ h4 {
     margin-bottom: 0.5rem;
     color: #fff;
 }
+.footer-top,
+.footer-bottom {
+    .container > .flex {
+        justify-content: space-between;
+    }
+}
 
 // *footer top section
 .footer-top {
     background: url("../assets/img/footer-bg.jpg");
-
-    .container > .flex {
-        justify-content: space-between;
-    }
 }
 .footer-top-left {
     padding: $footer-top-left-padding;
@@ -165,7 +205,7 @@ h4 {
         line-height: 0.7rem;
             a {
                 font-size: $secondary-font-size;
-                color: gray;
+                color: $tertiary-color;
 
                 &:hover {
                     text-decoration: underline;
@@ -174,7 +214,7 @@ h4 {
         }
     }
 }
-.footer-top-left-merch ul:first-child {
+.footer-top-left-merch .dc-comics {
     margin-bottom: 1rem;
 }
 .footer-top-right {
@@ -188,4 +228,31 @@ h4 {
 }
 
 // *footer bottom section
+.footer-bottom {
+    padding: $footer-bottom-padding;
+    background-color: #303030;
+    color: white;
+}
+.footer-bottom {
+    .container > .flex {
+        align-items: center;
+    }
+}
+.sites {
+    align-items: center;
+
+    > span:first-of-type {
+        font-size: 0.8rem;
+        font-weight: $main-font-weight;
+        color: $main-color;
+    }
+}
+.sites-links li {
+    margin-left: 0.8rem;
+    width: $footer-bottom-sites-img-width;
+
+    &:first-of-type {
+        margin-left: 1rem;
+    }
+}
 </style>
